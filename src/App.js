02cd5d3d63a905d91a1da3d94c81ddf37b2129a6
Santiago Liebrecht <santiago.liebrecht@wundermanthompson.com>
2022-11-08 11:44:03 +0000

    Added basic Component structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from "./logo.svg";
 import "./sass/main.scss";
+import Finder from "./components/Finder.js";
+import Random from "./components/Random.js";
+import Trending from "./components/Trending.js";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      {/* header div */}
+      <header>
+        <h1>React Giphy</h1>
       </header>
-    </div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="main">
+        <Random />
+        <Finder />
+        <Trending />
+      </div>
+    </main>
   );
 }
 
