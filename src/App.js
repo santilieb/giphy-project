967e78c2655c40026111733228cc659b8bfd092c
Santiago Liebrecht <santiago.liebrecht@wundermanthompson.com>
2022-11-09 15:08:34 +0000

    Added conditional rendering to components on window resize

--- src/App.js
+++ src/App.js
@@ -4,21 +4,57 @@ import Random from "./components/Random.js";
 import Trending from "./components/Trending.js";
 import Navbar from "./components/Navbar.js";
 import Header from "./components/Header";
+import { useContext, useState } from "react";
 import ProjectContext from "./context/ProjectContext";
-import { useContext } from "react";
 
 function App() {
   const { activeNav } = useContext(ProjectContext);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const renderContent = () => {
+    switch (activeNav) {
+      case "random":
+        return <Random />;
+      case "finder":
+        return <Finder />;
+      case "trending":
+        return <Trending />;
+      default:
+        return <Random />;
+    }
+  };
+
+  const renderContentDesktop = () => {
+    return (
+      <div className="main__content--desktop">
+        <Random />
+        <Finder />
+        <Trending />
+      </div>
+    );
+  };
+
+  //Function to handle the resize of the window
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  //Add the event listener to the window
+  window.addEventListener("resize", handleResize);
 
   return (
     <div className="App">
       <Header />
-      <Navbar />
 
       <main className="main">
-        {activeNav === "random" && <Random />}
-        {activeNav === "finder" && <Finder />}
-        {activeNav === "trending" && <Trending />}
+        {windowWidth < 1024 ? (
+          <>
+            <Navbar />
+            <div className="main__content">{renderContent()}</div>
+          </>
+        ) : (
+          <div className="main__content">{renderContentDesktop()}</div>
+        )}
       </main>
     </div>
   );
