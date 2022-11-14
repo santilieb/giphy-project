8762f0a42ccf48fd978099751d12ebf553114dec
Santiago Liebrecht <santiago.liebrecht@wundermanthompson.com>
2022-11-14 12:58:05 +0000

    Corrected one W3C standard error related to source

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -11,9 +11,8 @@ function GifItem({ smallSrcSet, largeSrcSet, alt }) {
         media="(max-width: 900px)"
         srcSet={`${smallSrcSet} 900w`}
         sizes="900px"
-        alt={alt}
       />
-      <source srcSet={largeSrcSet} alt={alt} />
+      <source srcSet={largeSrcSet} />
       {/* The img with src attribute is required for older browsers */}
       <img src={largeSrcSet} alt={alt} className="images-container__image" />
     </picture>
